820e88cd3736736a04ef198b14e39e6fba3f46d6
Hafiz Ismail <sgk.sprm@gmail.com>
2014-09-12 10:05:00 +0800

    Updated client build config for reactjs

--- client/config.js
+++ client/config.js
@@ -11,27 +11,45 @@ module.exports = {
         require: [
           { type: 'bower', name: 'angular' },
           { type: 'bower', name: 'angular-ui-router' },
+          { type: 'bower', name: 'angular-resource'},
+          { type: 'bower', name: 'angular-bootstrap'},
+          { type: 'bower', name: 'react', location: '../bower_components/react/react-with-addons.js'},
           { type: 'npm', name: 'lodash' },
+          { type: 'npm', name: 'url' },
           { type: 'npm', name: 'path' }
-        ]
-      },
-      {
-        name: 'path',
-        require: [
-          { type: 'npm', name: 'path' }
+        ],
+        transform: [
+          {
+            fn: 'browserify-shim',
+            opts: {
+              'angular-resource': 'angular-resource'
+            }
+          }
         ]
       },
       {
         name: 'apps.min',
         entries: 'src/apps/*/app.js',
         concat: true,
-        external: ['common']
+        external: ['common'],
+        transform: [
+          {
+            fn: 'reactify',
+            opts: {}
+          }
+        ]
       },
       {
         name: 'apps',
         entries: 'src/apps/*/app.js',
         concat: false,
-        external: ['common']
+        external: ['common'],
+        transform: [
+          {
+            fn: 'reactify',
+            opts: {}
+          }
+        ]
 
       }
     ]
